f652017ce394d0aa3650761d34d3a8e4bab53a50
milosz <git@miloszmilewski.dev>
2022-04-07 22:24:41 +0100

    exported friends card to a seperate file

--- lib/pages/friends.dart
+++ lib/pages/friends.dart
@@ -64,41 +64,55 @@ class _FriendsPageState extends State<FriendsPage> {
 
                         var data = snapshot.data!.data() as dynamic;
 
-                        return Card(
-                          clipBehavior: Clip.antiAlias,
-                          child: FittedBox(
-                            child: Stack(
-                              children: [
-                                Container(
-                                    foregroundDecoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                            colors: [
-                                              Colors.black.withOpacity(.5),
-                                              Colors.transparent
-                                            ],
-                                            begin: Alignment.bottomCenter,
-                                            end: Alignment.topCenter,
-                                            stops: const [0, 0.55])),
-                                    child: Image.network(data!["avatar"])),
-                                Positioned(
-                                    left: 3,
-                                    bottom: 3,
-                                    child: Text(
-                                      data!["displayName"],
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 8
-                                      ),
-                                    ))
-                              ],
-                            ),
-                            fit: BoxFit.fill,
-                          ),
-                        );
+                        return FavouriteCard(favouriteData: data);
                       });
                 });
           },
         ),
+      ),
+    );
+  }
+}
+
+class FavouriteCard extends StatelessWidget {
+  const FavouriteCard({
+    Key? key,
+    required this.favouriteData,
+  }) : super(key: key);
+
+  final favouriteData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: FittedBox(
+        child: Stack(
+          children: [
+            Container(
+                foregroundDecoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Colors.black.withOpacity(.5),
+                          Colors.transparent
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        stops: const [0, 0.55])),
+                child: Image.network(favouriteData!["avatar"])),
+            Positioned(
+                left: 3,
+                bottom: 3,
+                child: Text(
+                  favouriteData!["displayName"],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 8
+                  ),
+                ))
+          ],
+        ),
+        fit: BoxFit.fill,
       ),
     );
   }
