e129a1fc5beb9b2b72c6213f53c77ebcd0277b36
milosz <git@miloszmilewski.dev>
2022-04-10 02:02:09 +0100

    add functionality for removing friends

--- lib/component/FavouriteCard.dart
+++ lib/component/FavouriteCard.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FavouriteCard extends StatelessWidget {
   const FavouriteCard({
@@ -10,15 +13,28 @@ class FavouriteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var userReferece = FirebaseFirestore.instance
+        .collection("users")
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+
     var avatar = favouriteData["avatar"];
 
     return PopupMenuButton<int>(
       itemBuilder: (BuildContext context) => [
-        const PopupMenuItem(child: Text("Remove"), value: 0,)
+        const PopupMenuItem(
+          child: Text("Remove"),
+          value: 0,
+        )
       ],
       onSelected: (value) {
-        if(value == 0){
-
+        if (value == 0) {
+          userReferece.update({
+            "friends": FieldValue.arrayRemove([favouriteData['id']])
+          }).then((value) {
+            Get.rawSnackbar(message: "Successfully removed a friend");
+          }).catchError((err) {
+            Get.rawSnackbar(message: "${(err as FirebaseException).message}");
+          }, test: (e) => e is FirebaseException);
         }
       },
       elevation: 8,
@@ -37,16 +53,15 @@ class FavouriteCard extends StatelessWidget {
                           begin: Alignment.bottomCenter,
                           end: Alignment.topCenter,
                           stops: const [0, 0.55])),
-                  child: avatar != null ? Image.network(avatar) : const Icon(Icons.person)),
+                  child: avatar != null
+                      ? Image.network(avatar)
+                      : const Icon(Icons.person)),
               Positioned(
                   left: 3,
                   bottom: 3,
                   child: Text(
                     favouriteData["displayName"],
-                    style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 8
-                    ),
+                    style: const TextStyle(color: Colors.white, fontSize: 8),
                     textScaleFactor: 1,
                   ))
             ],
