a2c18f6ed1a30b01f287e91fd3f3f93a82b6e739
milosz <git@miloszmilewski.dev>
2022-04-10 01:34:33 +0100

    add popup menu button to the favourite card

--- lib/component/FavouriteCard.dart
+++ lib/component/FavouriteCard.dart
@@ -10,35 +10,49 @@ class FavouriteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: FittedBox(
-        child: Stack(
-          children: [
-            Container(
-                foregroundDecoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [
-                          Colors.black.withOpacity(.5),
-                          Colors.transparent
-                        ],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                        stops: const [0, 0.55])),
-                child: Image.network(favouriteData!["avatar"])),
-            Positioned(
-                left: 3,
-                bottom: 3,
-                child: Text(
-                  favouriteData!["displayName"],
-                  style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 8
-                  ),
-                ))
-          ],
+    var avatar = favouriteData["avatar"];
+
+    return PopupMenuButton<int>(
+      itemBuilder: (BuildContext context) => [
+        const PopupMenuItem(child: Text("Remove"), value: 0,)
+      ],
+      onSelected: (value) {
+        if(value == 0){
+
+        }
+      },
+      elevation: 8,
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: FittedBox(
+          child: Stack(
+            children: [
+              Container(
+                  foregroundDecoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            Colors.black.withOpacity(.5),
+                            Colors.transparent
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                          stops: const [0, 0.55])),
+                  child: avatar != null ? Image.network(avatar) : const Icon(Icons.person)),
+              Positioned(
+                  left: 3,
+                  bottom: 3,
+                  child: Text(
+                    favouriteData["displayName"],
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 8
+                    ),
+                    textScaleFactor: 1,
+                  ))
+            ],
+          ),
+          fit: BoxFit.fill,
         ),
-        fit: BoxFit.fill,
       ),
     );
   }
